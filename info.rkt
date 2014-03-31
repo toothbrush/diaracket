d07b964a028d5d56117de1a34609a50d7256f938
vanderwa <vanderwa@5865ca3f-f427-0410-b179-9d25b408ab24>
2014-03-31 15:43:48 +0000

    turning this into a usable package
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/phoenix/people/paul/Docs/20130119-programming-framework/example-implementations/diaracket@16243 5865ca3f-f427-0410-b179-9d25b408ab24

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "diaracket")
+(define version "0.1.1")
+(define source-omit-files (list "FRTDiaspec.rkt"
+                                "typed-sketch-impl.rkt"
+                                "typed-sketch.rkt.mock"
+                                "proto-spec.txt"
+                                "proto-imp-proxy.txt"))
