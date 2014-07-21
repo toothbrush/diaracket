fbcbd331aa38b65d6e6fd319e049fe878401e77f
vanderwa <vanderwa@5865ca3f-f427-0410-b179-9d25b408ab24>
2014-07-21 15:01:29 +0000

    very minor cosmetics
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/phoenix/people/paul/Docs/20130119-programming-framework/example-implementations/diaracket@16456 5865ca3f-f427-0410-b179-9d25b408ab24

--- evilcam-spec.rkt
+++ evilcam-spec.rkt
@@ -3,14 +3,14 @@
 ; the sources and actions should be defined elsewhere, in a central taxonomy.
 (taxonomy "taxo.rkt")
 
-(define-context MakeAd String [when-required get IP])
+(define-context MakeAd String          [when-required get IP])
 
 (define-context ProcessPicture Picture [when-provided Button
                                                       get Camera
                                                       maybe_publish])
 
-(define-context DisplayApp Picture   [when-provided ProcessPicture 
-                                                    get MakeAd 
-                                                    always_publish])
+(define-context DisplayApp Picture     [when-provided ProcessPicture 
+                                                      get MakeAd 
+                                                      always_publish])
 
-(define-controller ScreenController [when-provided DisplayApp do Screen])
+(define-controller ScreenController    [when-provided DisplayApp do Screen])
