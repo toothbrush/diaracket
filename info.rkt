ae79a11dcf2d974c440fac13c544fec643a81df3
vanderwa <vanderwa@5865ca3f-f427-0410-b179-9d25b408ab24>
2015-02-24 13:30:41 +0000

    lots of plumbing, reshuffling source files around.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/phoenix/people/paul/Docs/20130119-programming-framework/example-implementations/diaracket@16953 5865ca3f-f427-0410-b179-9d25b408ab24

--- info.rkt
+++ info.rkt
@@ -1,8 +1,4 @@
 #lang info
 (define collection "diaracket")
-(define version "0.1.3")
-(define source-omit-files (list "FRTDiaspec.rkt"
-                                "typed-sketch-impl.rkt"
-                                "typed-sketch.rkt"
-                                "proto-spec.txt"
-                                "proto-imp-proxy.txt"))
+(define version "0.1.4")
+(define source-omit-files (list "experiments"))
