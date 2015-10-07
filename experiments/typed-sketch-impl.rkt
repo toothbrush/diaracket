1bb1ca0ee2ceb3ed77a28325a12da06b29637cce
vanderwa <vanderwa@5865ca3f-f427-0410-b179-9d25b408ab24>
2015-10-07 17:14:28 +0000

    move example-implementations up a few levels.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/phoenix/people/paul/Docs/20130119-programming-framework/example-implementations/diaracket@18002 5865ca3f-f427-0410-b179-9d25b408ab24

--- experiments/typed-sketch-impl.rkt
+++
@@ -1,3 +0,0 @@
-#lang s-exp "typed-sketch.rkt"
-
-(implement fanspeed (lambda (x) (display "speed")(display x)(newline) "asdf?" 24))
--- info.rkt
+++
@@ -1,4 +0,0 @@
-#lang info
-(define collection "diaracket")
-(define version "0.1.4")
-(define source-omit-files (list "experiments"))
